4c8226c2b79fc8800339d3d6af87fb4db8df05b5
sp3ncerlan <lansp329@gmail.com>
2025-12-19 19:01:02 -0600

    feat: implement core flow with Redis locking
    - Added pessimistic locking in repository method for another layer of concurrency guarding

--- src/main/java/com/spencer/distributed_job_scheduler/repository/JobRepository.java
+++ src/main/java/com/spencer/distributed_job_scheduler/repository/JobRepository.java
@@ -3,11 +3,14 @@ package com.spencer.distributed_job_scheduler.repository;
 import com.spencer.distributed_job_scheduler.model.Job;
 import com.spencer.distributed_job_scheduler.model.JobStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import jakarta.persistence.LockModeType;
 import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<Job, UUID> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Job> findTopByStatusAndScheduledAtBeforeOrderByScheduledAtAsc(JobStatus status, Instant time);
 }
